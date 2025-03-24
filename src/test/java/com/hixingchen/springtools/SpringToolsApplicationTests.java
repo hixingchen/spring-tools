7321957f885c41a5f42e60755478f0a9f2847fbd
hixingchen <2781003217@qq.com>
2025-03-24 17:57:39 +0800

    初始化项目

---
+++ src/test/java/com/hixingchen/springtools/SpringToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hixingchen.springtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
